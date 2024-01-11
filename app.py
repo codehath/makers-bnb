07a71b36845757be7292032e70bd224f9381e9a8
codehath <codingfarhath@gmail.com>
2024-01-11 03:33:52 +0000

    # Added calendar with jinja variables to space booking page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import os#, datetime
+import os  # , datetime
 from datetime import datetime
 from flask import Flask, request, render_template
 from lib.database_connection import get_flask_database_connection
@@ -24,13 +24,15 @@ db = PostgresqlDatabase(
 # Connect to the database
 db.connect()
 
+
 # Function to convert form date inputs into datetime objects...
 # ...so that they can be compared against table DateFields
 def date_conv(date):
-    return datetime.strptime(date, '%Y-%m-%d')
+    return datetime.strptime(date, "%Y-%m-%d")
 
 
 # == Your Routes Here ==
+
 
 # GET /index
 # Returns the homepage
@@ -41,26 +43,55 @@ def get_index():
     return render_template("index.html")
 
 
-@app.route('/spaces', methods=['GET'])
+@app.route("/spaces", methods=["GET"])
 def spaces():
     spaces = Space.select()
-    return render_template('spaces.html', spaces=spaces)
+    return render_template("spaces.html", spaces=spaces)
 
 
-@app.route('/spaces', methods=['POST'])
+@app.route("/spaces", methods=["POST"])
 def spaces_date_range():
     # Join availability table to space table and only select spaces with availability between the dates entered in the form
-    spaces = Space.select().join(Availability).where(date_conv(request.form['avail-from']) <= Availability.start_date and date_conv(request.form['avail-to']) >= Availability.end_date)
-    return render_template('spaces.html', spaces=spaces)
+    spaces = (
+        Space.select()
+        .join(Availability)
+        .where(
+            date_conv(request.form["avail-from"]) <= Availability.start_date
+            and date_conv(request.form["avail-to"]) >= Availability.end_date
+        )
+    )
+    return render_template("spaces.html", spaces=spaces)
 
 
-@app.route('/spaces/<int:id>', methods=['GET'])
+@app.route("/spaces/<int:id>", methods=["GET"])
 def get_album(id):
     space = Space.select().where(id == id)
-    return render_template('space.html', space=space[0])
 
+    # min_date should be today
+    # max_date should be the last bookable date/month/year for that space
 
+    # available_dates
+    # All the availability dates
 
+    # booked_dates
+    # Get all bookings for this space
+    # Get all dates for those bookings
+    # store those in a list
+    # if start date == end date, store start date
+    # else store range as a pair
+
+    booked_dates = [
+        "2024-01-02",
+        ["2024-01-06", "2024-01-11"],
+        "2024-01-18",
+        "2024-01-19",
+        "2024-01-20",
+        "2024-01-25",
+        "2024-01-28",
+    ]
+    return render_template("space_cal.html", space=space[0], booked_dates=booked_dates)
+
+    return render_template("calendar.html", booked_dates=booked_dates)
 
 
 # These lines start the server if you run this file directly
