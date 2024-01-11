2ce707f63156c27c9fa1da36f19f668e5377ab29
“Leah” <lnsimonn@hotmail.com>
2024-01-11 17:42:04 +0000

    pass in database info to send_email and send_sms

--- lib/send_email.py
+++ lib/send_email.py
@@ -1,8 +1,6 @@
 import requests
-#from creds import MAILGUN_DOMAIN, MAILGUN_API_KEY
+from creds import MAILGUN_DOMAIN, MAILGUN_API_KEY
 
-MAILGUN_API_KEY = '505fe23a102dbd5d37dd6d8f6c262c48-7ecaf6b5-0e4937a0'
-MAILGUN_DOMAIN = 'sandbox8b4c55b97ded45258c9e7b59df25d875.mailgun.org'
 
 def send_email(to_email, subject, body):
     mailgun_url = f'https://api.mailgun.net/v3/{MAILGUN_DOMAIN}/messages'
@@ -19,48 +17,48 @@ def send_email(to_email, subject, body):
     
     print(f"Email sent with status code: {response.status_code} and {response.content}")
 
-def signup_email(user_email):
+def signup_email(person):
     subject = 'Welcome to MakersBnb'
     body = f'Thank you for signing up with MakersBnB. We are excited to have you on board!'
-    send_email(user_email, subject, body)
+    send_email(person.email, subject, body)
 
-def request_space_created(user_email, space_name):
+def request_space_created(person, space):
     subject = 'Space Created'
-    body = f'Congratulations! You have successfully created a space named {space_name}.'
-    send_email(user_email, subject, body)
+    body = f'Congratulations! You have successfully created a space named {space.name}.'
+    send_email(person.email, subject, body)
 
-def request_space_updated(user_email, space_name):
+def request_space_updated(person, space):
     subject = 'Space Updated'
-    body = f'The details of your space {space_name} have been updated. Check the changes and ensure they meet your requirements.'
-    send_email(user_email, subject, body)
+    body = f'The details of your space {space.name} have been updated. Check the changes and ensure they meet your requirements.'
+    send_email(person.email, subject, body)
 
-def request_to_book(user_email, space_name, start_date, end_date):
+def request_to_book(person, space, booking):
     subject = 'Booking Request Received'
-    if start_date == end_date:
-        body = f'Thank you for your booking request for {space_name} on {start_date}. We will review it shortly.'
+    if booking.start_date == booking.end_date:
+        body = f'Thank you for your booking request for {space.name} on {booking.start_date}. We will review it shortly.'
     else:
-        body = f'Thank you for your booking request for {space_name} from {start_date} to {end_date}. We will review it shortly.'
-    send_email(user_email, subject, body)
+        body = f'Thank you for your booking request for {space.name} from {booking.start_date} to {booking.end_date}. We will review it shortly.'
+    send_email(personal.email, subject, body)
 
-def booking_request_pending(user_email, space_name):
+def booking_request_pending(person, space):
     subject = 'Your Booking Request is Pending!'
-    body = f'Thank you for requesting to book {space_name} on [Your Platform]. The host will review your request shortly.'
-    send_email(subject, body, user_email)
+    body = f'Thank you for requesting to book {space.name} on [Your Platform]. The host will review your request shortly.'
+    send_email(subject, body, person.email)
 
-def booking_confirmed(user_email, space_name, booking_date):
+def booking_confirmed(person, space, booking):
     subject = 'Booking Confirmed'
-    body = f'Congratulations! Your booking request for {space_name} on {booking_date} has been confirmed.'
-    send_email(user_email, subject, body)
+    body = f'Congratulations! Your booking request for {space.name} on {booking.start_date} has been confirmed.'
+    send_email(person.email, subject, body)
 
-def booking_denied(user_email, space_name, booking_date):
+def booking_denied(perosn, space, booking):
     subject = 'Booking Denied'
-    body = f'We regret to inform you that your booking request for {space_name} on {booking_date} has been denied.'
-    send_email(user_email, subject, body)
+    body = f'We regret to inform you that your booking request for {space.name} on {booking.start_date} has been denied.'
+    send_email(person.email, subject, body)
 
-def booking_request(host_email, space_name):
+def booking_request(person, space):
     subject = 'New Booking Request for Your Space!'
-    body = f'You have received a booking request for your space ({space_name}) on [Your Platform]. Please review and respond.'
-    send_email(subject, body, host_email)
+    body = f'You have received a booking request for your space ({space.name}) on [Your Platform]. Please review and respond.'
+    send_email(subject, body, person.email)
 
 
 
--- lib/send_sms.py
+++ lib/send_sms.py
@@ -1,9 +1,5 @@
 from twilio.rest import Client
-
-account_sid = 'ACd5d7bda44df88fcdaeb7fea20e7b2747'
-auth_token = '18d0d863dd7b2674527884d8d12a17e6'
-twilio_phone_number = '+447400361560'
-recipient_phone_number = '+447943092053' 
+from creds import *
 
 client = Client(account_sid, auth_token)
 
@@ -17,17 +13,17 @@ def send_booking_sms(client, recipient_phone_number,  message_body):
     print(f"Message SID: {message.sid}")
 
 # booking_status, person_name, space_name, booking_date
-def requested_text(person_name, space_name, booking_date):
-    message_body = f"{person_name} has requested to book {space_name} on {booking_date}. Please review the request."
-    send_booking_sms(client, person.Number, message_body)
+def requested_text(person, space, booking):
+    message_body = f"{person.name} has requested to book {space.name} on {booking.start_date}. Please review the request."
+    send_booking_sms(client, person.phone_number, message_body)
 
-def requested_text_confirmed(space_name, booking_date):
-    message_body = f"Great news! Your booking request for {space_name} on {booking_date} has been confirmed. We look forward to welcoming you!"
-    send_booking_sms(client, person.Number, message_body)
+def requested_text_confirmed(space, booking):
+    message_body = f"Great news! Your booking request for {space.name} on {booking.start_date} has been confirmed. We look forward to welcoming you!"
+    send_booking_sms(client, person.phone_number, message_body)
 
-def requested_text_denied(space_name, booking_date):
-    message_body = f"We regret to inform you that your booking request for {space_name} on {booking_date} has been denied. Please contact us for more information."
-    send_booking_sms(client, person.Number, message_body)
+def requested_text_denied(space, booking):
+    message_body = f"We regret to inform you that your booking request for {space.name} on {booking.start_date} has been denied. Please contact us for more information."
+    send_booking_sms(client, person.phone_number, message_body)
 
 
     
